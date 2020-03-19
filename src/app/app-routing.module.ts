b74431a264f61cdef739f7ae799d7be0b51cde07
sumanpaul2031@gmail.com <sumanpaul2031@gmail.com>
2020-03-19 23:30:16 +0530

    Final Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'mainpage' },
+  { path: 'mainpage', component: MainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
